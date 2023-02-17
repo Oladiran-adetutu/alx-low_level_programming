484d944e1963235fa945a49073e2bbf432b4babf
oladiran-adetutu <oladiranadetutu145@gmail.com>
2023-02-17 05:33:24 -0800

    task 7

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - this is the main function
+ * main - my new main function
  *
- * Return: 0 when sucessful
- * /
- 
+ * Return: 0 when completed
+ */
+
 int main(void)
 {
-      printf("with proper grammar, but the outcome is a piec e of art,\n");
-      return (0);
+        printf("with proper grammar, but the outcome is a piece of art,\n"); 
+	return (0);
 }

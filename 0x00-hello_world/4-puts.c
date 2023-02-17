a0b19248cd4f24955a9a60d6ae2aa43a87862496
oladiran-adetutu <oladiranadetutu145@gmail.com>
2023-02-17 04:09:52 -0800

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,14 @@
-#include <stdio.h>   
+#include <stdio.h>
 
 /**
- * main - this is the function
+ * main - my main function
  *
  * Return: always 0
- */ 
+ */
 
 int main(void)
 {
-	'Puts'("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-} 
+
+}

fb8dd0504ae8e380457462022af0315ad7813a19
oladiran-adetutu <oladiranadetutu145@gmail.com>
2023-02-18 03:24:28 -0800

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,12 +2,11 @@
 
 /**
  * main - main function of mine
- *
- * Return:0 on final
+ * Return: Always 0.
  */
 
 int main(void)
-{ 
+{
 	printf("Size of a char: %lu byte(s)", Sizeof(char));
 	printf("Size of an int: %lu byte(s)", Sizeof(int));
 	printf("Size of a long int: %lu byte(s)", Sizeof(long int));
@@ -19,5 +18,4 @@ int main(void)
 	printf("Size of a long long int: %lu byte(s)", Sizeof(long long int));
 	printf("Size of a float: %lu byte(s)", Sizeof(float));
 	return (0);
-	
 }

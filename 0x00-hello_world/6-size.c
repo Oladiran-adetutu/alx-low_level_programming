1553d4731ede378e8682c8003db5fca20fa02571
oladiran-adetutu <oladiranadetutu145@gmail.com>
2023-02-19 04:44:42 -0800

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,5 +12,6 @@ int main(void)
 	printf("Size of a long int: %lu byte(s)", sizeof(long int));
 	printf("Size of a long long int: %lu byte(s)", sizeof(long long int));
 	printf("Size of a float: %lu byte(s)", sizeof(float));
+	printf("\n");
 	return (0);
 }

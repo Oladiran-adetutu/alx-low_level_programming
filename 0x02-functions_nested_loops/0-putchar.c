38771533c268ba8f37bdfc2f122538461e3159f7
oladiran-adetutu <oladiranadetutu145@gmail.com>
2023-02-21 08:42:59 -0800

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include 'main.h'
 /**
  * main - prints putchar
  * Return: Always 0
@@ -6,7 +6,7 @@
 
 int main(void)
 {
-	_putchar(`_`);
+	_putchar('_');
 	_putchar('p');
 	_putchar('u');
 	_putchar('t');

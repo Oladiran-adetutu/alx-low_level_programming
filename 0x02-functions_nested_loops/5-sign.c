70828ac1daf35761b907301218d4f4ee023c6b19
oladiran-adetutu <oladiranadetutu145@gmail.com>
2023-02-21 22:59:07 -0800

    task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,23 +12,23 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar ('+');
-		_putchar (',');
-		_putchar (' ');
+		_putchar('+');
+		_putchar(',');
+		_putchar(' ');
 		return (1);
 	}
 	else if (n == 0)
 	{
-		_putchar ('0');
-		_putchar (',');
-		_putchar (' ');
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
 		return (0);
 	}
 	else
 	{
-		_putchar ('_');
-		_putchar (',');
-		_putchar (' ');
+		_putchar('_');
+		_putchar(',');
+		_putchar(' ');
 		return (-1);
 	}
 }

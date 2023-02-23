212aed149ddb73ef871dd9f115d8f7fe126617bb
oladiran-adetutu <oladiranadetutu145@gmail.com>
2023-02-23 01:13:21 -0800

    task 0

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * positive_or_negative - checks for positive or negative numbers
+ *
+ * @i: the number to be checked
+ *
+ * Return: Always 0
+ */
+
+void positive_or_negative(int i);
+{
+	if (i < 98)
+		printf("%d is negative\n", i);
+	else if (i < 98)
+		printf("%d is positive\n", i);
+}

f90dcd705f7a45d7497660f123e1d8e100a0a700
oladiran-adetutu <oladiranadetutu145@gmail.com>
2023-02-24 13:45:15 -0800

    task 2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -30,32 +30,32 @@ int largest_number(int num1, int num2, int num3)
 	{
 		largest = num2;
 	}
-	else if
+	else if
 	 (num3 > num1 && num3 > num2)
 	{
 		largest = num3;
 	}
-	else if (num1 == num2 && num1 > num3)
+	else if (num1 == num2 && num1 > num3)
 	{
 		largest = num1;
 	}
-	else if (num1 == num3 && num1 > num2)
+	else if (num1 == num3 && num1 > num2)
 	{
 		largest = num1;
 	}
-	else if (num2 == num3 && num2 > num1)
+	else if (num2 == num3 && num2 > num1)
 	{
 		largest = num2;
 	}
-	else if (num2 == num1 && num2 > num3)
+	else if (num2 == num1 && num2 > num3)
 	{
 		largest = num2;
 	}
-	else if (num3 == num1 && num3 > num2)
+	else if (num3 == num1 && num3 > num2)
 	{
 		largest = num3;
 	}
-	else if (num3 == num2 && num3 > num1)
+	else if (num3 == num2 && num3 > num1)
 	{
 		largest = num3;
 	}

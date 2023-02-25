e4c3cf9956d11998ac3ae15d3247cef46ddb8b35
oladiran-adetutu <oladiranadetutu145@gmail.com>
2023-02-25 04:15:53 -0800

    task 4

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * jack_bauer - function declaration
+ * Description: prints avery minute 
+ * Return: void
+ */
+
+void jack_bauer(void);
+
+/**
+ * jack_bauer - function defination
+ * Description: prints every single minute
+ * Return: void
+ */
+

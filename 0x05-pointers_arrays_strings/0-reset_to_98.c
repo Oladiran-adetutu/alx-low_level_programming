65b86c517c9c36dd4e115c804b1f42873e60e4ff
oladiran-adetutu <oladiranadetutu145@gmail.com>
2023-02-27 12:35:37 -0800

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function declaration
+ * @n: pointer integer
+ * Return: void
+ */
+
+void reset_to_98(int *n);
+
+/**
+ * reset_to_98 - function defination
+ * Description: reseta value to 98
+ * @n: pointer integer
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
+

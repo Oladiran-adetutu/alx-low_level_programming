b6a1845cf6300eee181f0eba298d3378cfa29b7e
oladiran-adetutu <oladiranadetutu145@gmail.com>
2023-03-04 04:06:40 -0800

    task 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strncat - adds two strings
+ * @dest: string to be appended on
+ * @src: string to be appended to dest
+ * @n: number of bytes from src
+ * Return: pointer to the resulting dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+
+{
+
+	int index = 0, dest_len = 0;
+
+	while (dest[index++])
+	dest_len++;
+	for (index = 0; src[index] && index < n; index ++)
+	dest[dest_len++] = src[index];
+	return (dest);
+
+}

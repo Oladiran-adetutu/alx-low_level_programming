7f53f9e716e73ff50f636ea8007d5c099918d5f9
oladiran-adetutu <oladiranadetutu145@gmail.com>
2023-03-20 15:26:05 -0700

    task 3

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -3,7 +3,7 @@
 
 /*
  * File: 3-function_like_macro.h
- * Description: Defines a function-like macro ABS(x)
+ * Desc: Defines a function-like macro ABS(x)
  * that computes the absolute value of a complete number x.
  */
 

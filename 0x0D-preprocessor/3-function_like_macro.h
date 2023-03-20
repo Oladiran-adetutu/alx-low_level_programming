45ec66517ce3afe364d19dd9dee48cf67274a184
oladiran-adetutu <oladiranadetutu145@gmail.com>
2023-03-20 14:50:00 -0700

    task 3

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,12 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
+/*
+ * file: 3-function_like_macro.h
+ * Description: Defines a function_like macro ABS(x)
+ * that computs the absolute value of a complate number x.
+ */
+
+#define ABS(x) ((x) < 0 ? (-x) : (x))
+
+#endif

51cb0b81deec4084a3b9e108f1fde6719393cb68
oladiran-adetutu <oladiranadetutu145@gmail.com>
2023-03-30 13:46:20 -0700

    task 100

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * first - prints a sentence before main.c
+ * function is executed
+ */
+
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}

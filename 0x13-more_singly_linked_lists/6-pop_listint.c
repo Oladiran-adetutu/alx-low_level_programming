72213c347184c484d5e9b5d9be7d478d80e17a23
oladiran-adetutu <oladiranadetutu145@gmail.com>
2023-04-04 00:05:03 -0700

    6-pop_listint.c

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * pop_listint - pops node
+ * @head: list address
+ * Return: head node date
+ */
+
+int pop_listint(listint_t **head)
+{
+	int value = 0;
+	listint_t *tmp;
+
+	if (*head == NULL)
+		return (value);
+	tmp = *head;
+	value = tmp->n;
+	free(*head);
+	*head = tmp->next;
+
+	return (value);
+}
+
+
